2e3aad5a0257bf44bc8b4668eee1eef21e9eb094
Rich <richardstrain@gmail.com>
2024-07-30 15:56:18 -0400

    Add Role

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ db.connect((err) => {
   mainMenu();
 });
 
-// Function to display the main menu
+// Function to display the initial main menu
 function mainMenu() {
   inquirer
     .prompt([
@@ -28,6 +28,7 @@ function mainMenu() {
       },
     ])
     .then((answer) => {
+      // depending on the user's choice, call the appropriate function to handle the needed action
       switch (answer.choice) {
         case 'View All Departments':
           viewAllDepartments();
@@ -57,7 +58,7 @@ function mainMenu() {
     });
 }
 
-// View all departments
+// Display All Departments In The Terminal
 const viewAllDepartments = () => {
   db.query('SELECT * FROM department', (err, res) => {
     if (err) throw err;
@@ -66,6 +67,7 @@ const viewAllDepartments = () => {
   });
 };
 
+// Display All Roles In The Terminal
 const viewAllRoles = () => {
   db.query(
     'SELECT role.id, role.title, department.name as department, role.salary FROM role JOIN department ON role.department_id = department.id;',
@@ -77,7 +79,7 @@ const viewAllRoles = () => {
   );
 };
 
-// Needs to be fixed - Manager Name is not displaying
+// Display All Employees In The Terminal
 const viewAllEmployees = () => {
   db.query(
     `SELECT e.id, e.first_name, e.last_name, r.title, d.name as department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e JOIN role r ON e.role_id = r.id JOIN department d ON r.department_id = d.id LEFT JOIN employee m ON e.manager_id = m.id;`,
@@ -91,6 +93,7 @@ const viewAllEmployees = () => {
 
 const addEmployee = () => {};
 
+// Create New Department
 const addDepartment = () => {
   inquirer
     .prompt([
@@ -101,14 +104,55 @@ const addDepartment = () => {
       },
     ])
     .then((answer) => {
+      // Insert the new department into the department table, using $1 as a placeholder to prevent SQL injection
       db.query('INSERT INTO department (name) VALUES ($1)', [answer.department], (err, res) => {
         if (err) throw err;
-        console.log(`\n${answer.department} added to the department table.`);
+        console.log(`${answer.department} added to the department table. \n`);
         mainMenu();
       });
     });
 };
 
-const addRole = () => {};
+// Add employee role, role consist of title, salary and department
+const addRole = () => {
+  // Get all departments to display as choices
+  db.query('SELECT * FROM department', (err, res) => {
+    if (err) throw err;
+    // create an array of department objects to use as choices
+    const departments = res.rows.map((department) => {
+      return {
+        name: department.name,
+        value: department.id,
+      };
+    });
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'title',
+          message: 'Enter the title of the role:',
+        },
+        {
+          type: 'input',
+          name: 'salary',
+          message: 'Enter the salary of the role:',
+        },
+        {
+          type: 'list',
+          name: 'department_id',
+          message: 'Select the department for the role:',
+          choices: departments,
+        },
+      ])
+      .then((answer) => {
+        // Insert the new department into the department table, using $1, $2, $3 as a placeholders to prevent SQL injection
+        db.query('INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)', [answer.title, answer.salary, answer.department_id], (err, res) => {
+          if (err) throw err;
+          console.log(`${answer.title} added to the role table. \n`);
+          mainMenu();
+        });
+      });
+  });
+};
 
 const updateEmployeeRole = () => {};
